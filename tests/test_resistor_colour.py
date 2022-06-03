27a8f3dff3d52289b0016cb2bf9fdd2a28aeccf0
Angelica <Angelica137@users.noreply.github.com>
2022-06-03 20:25:24 +0100

    test returns the band colours, red

--- tests/test_resistor_colour.py
+++ tests/test_resistor_colour.py
@@ -5,3 +5,9 @@ def test_resistor_colour_0():
     m = ResistorColour()
     assert m.band_value("black") == 0
     assert m.band_value("blue") == 6
+
+
+def test_band_colours_returns_colours():
+    m = ResistorColour()
+    assert m.band_colours() == ["black", "brown", "red", "orange", "yellow",
+                                "green", "blue", "violet", "grey", "white"]
